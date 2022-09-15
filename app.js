ac121c44b394e36daf0308621b89694b4f6f1092
Anastasia Andreeva <nastiy1890@yandex.ru>
2022-09-14 17:46:23 -0700

    feature: added cors

--- app.js
+++ app.js
@@ -12,13 +12,13 @@ const error = require('./errors/errors');
 const auth = require('./middlewares/auth');
 const { validateSignUp, validateSignIn } = require('./middlewares/validators');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-//const cors = require('./middlewares/cors');
+const cors = require('./middlewares/cors');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-//app.use(cors);
+app.use(cors);
 
 app.use(helmet());
 app.disable('x-powered-by');
@@ -29,11 +29,11 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use(requestLogger);
 
-/* app.get('/crash-test', () => {
+app.get('/crash-test', () => {
   setTimeout(() => {
     throw new Error('Сервер сейчас упадёт');
   }, 0);
-}); */
+});
 
 app.post('/signup', validateSignUp, createUser);
 app.post('/signin', validateSignIn, login);
