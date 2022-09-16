b9492ccb0ebc95037c7417f1205aad76ef7a99bb
Anastasia Andreeva <nastiy1890@yandex.ru>
2022-09-15 21:35:15 -0700

    fix

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
-//const cors = require('cors');
+const cors = require('cors');
 require('dotenv').config();
 
 const bodyParser = require('body-parser');
@@ -20,7 +20,7 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-//app.use(cors());
+app.use(cors());
 
 app.use(helmet());
 app.disable('x-powered-by');
